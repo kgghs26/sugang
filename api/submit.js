3a8c4ce312946c806330b2e67f498ebfb7525037
csolutn <115399290+csolutn@users.noreply.github.com>
2026-07-02 16:16:15 +0900

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -10,7 +10,7 @@ function verify(req){
 
 export default async function handler(req, res){
   // ===== CORS =====
-  res.setHeader("Access-Control-Allow-Origin", "https://csolutn.github.io");
+  res.setHeader("Access-Control-Allow-Origin", "https://kgghs26.github.io");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") return res.status(200).end();

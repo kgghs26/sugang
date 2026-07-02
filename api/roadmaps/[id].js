4a8053f3a084f42c6dfcb0da16e9c63391c054e7
csolutn <115399290+csolutn@users.noreply.github.com>
2026-07-02 16:16:28 +0900

    Update [id].js

--- api/roadmaps/[id].js
+++ api/roadmaps/[id].js
@@ -10,7 +10,7 @@ function verify(req){
 
 export default async function handler(req, res){
   // ===== CORS 허용 =====
-  res.setHeader("Access-Control-Allow-Origin", "https://csolutn.github.io");
+  res.setHeader("Access-Control-Allow-Origin", "https://kgghs26.github.io");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") return res.status(200).end();  // preflight 응답
